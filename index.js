866ab7327d946ac9397895302cc884831c1dfa53
Tiago rosa da costa <tiagorosadacost@gmail.com>
2022-07-15 20:15:31 -0300

    change to get tweets previous day

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const axios = require("axios")
 const Twitter = require("twitter-v2")
 const schedule = require('node-schedule');
+const moment = require("moment");
 
 
 const client = new Twitter({
@@ -24,11 +25,11 @@ function sleep(seconds) {
     })
 }
 
-async function getTweetsTodayByProfile(profile) {
+async function getTweetsTodayByProfile(profile, initDate, endDate) {
     const response = await client.get("tweets/search/recent", {
         query: `from:${profile}`,
-        "start_time": '2022-07-11T00:00:00Z',
-        "end_time": '2022-07-11T23:59:59Z',
+        "start_time": initDate,
+        "end_time": endDate,
         "tweet.fields": "created_at",
         "expansions": "author_id"
     })
@@ -41,12 +42,13 @@ async function getTweetsTodayByProfile(profile) {
     let links = []
     for (let index = 0; index < response.data.length; index++) {
 
-        if (links.length == 5) {
+        if (links.length == 20) {
             await axios.post(process.env.URL_WEBHOOK_CHANNEL_DISCORD, {
                 content: links.join("\n")
             })
             console.log(`Sended ${links.length} tweets made of ${profile} to Discord`)
             links = []
+            await sleep(1)
         }
 
         const item = response.data[index]
@@ -61,9 +63,11 @@ async function getTweetsTodayByProfile(profile) {
     }
 }
 
-schedule.scheduleJob('30 23 * * *', async () => {
+schedule.scheduleJob('30 0 * * *', async () => {
+    const initDate = (moment().utc().subtract(1, "day").format("YYYY-MM-DDT00:00:00Z"))
+    const endDate =  (moment().utc().subtract(1, "day").format("YYYY-MM-DDT23:59:59Z"))
     for (let index = 0; index < profilesToMonitor.length; index++) {
-        await getTweetsTodayByProfile(profilesToMonitor[index])
+        await getTweetsTodayByProfile(profilesToMonitor[index], initDate, endDate)
         sleep(1)
     }
 })
